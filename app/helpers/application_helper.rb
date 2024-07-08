729bd438ad84e9b8cef306456fca9dc68f2cb702
deepak raghuwanshi <deepakraghuwanshi0491@gmail.com>
2024-07-08 21:53:25 +0530

    manage saved item and added on project and jobs page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def my_saved_item?(item_type, item_id)
+    return false unless current_user
+
+    current_user.saved_items.find_by(item_type: item_type.classify, item_id: item_id).present?
+  end
 end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,7 +1,2 @@
 module ProjectsHelper
-  def my_saved_project?(project_id)
-    return false unless current_user
-
-    current_user.saved_items.find_by(item_type: 'Project', item_id: project_id).present?
-  end
 end
